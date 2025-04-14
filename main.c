27b167dccd8c7fd1a2c7d373112e7d689435deda
AndLof <andrea.loforese96@gmail.com>
2025-04-14 10:25:23 +0200

    prova assignment2

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 #include "timer.h"
 #include "uart.h"
 
-// Assignment 1
+/* Assignment 1
 
 int main(void) {
     ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000; // Disabilita gli ingressi analogici
@@ -32,6 +32,43 @@ int main(void) {
     
     while(1){
         
+    }
+    return 0;
+}
+*/
+
+//Assignment 2
+
+int main(void) {
+    ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000; // Disabilita gli ingressi analogici
+    
+    spi_init();
+    
+    //Make the magnetometer switch to Sleep mode; then make it go to active mode; 
+    mag_enable();
+    
+    CircularBuffer cb;
+    cb_init(&cb);
+    
+    unsigned int read_addr = 0x42;
+    uint8_t int value1;
+    uint8_t int value2;
+    unsigned int value;
+    //unsigned int chip_id = spi_write(read_addr);
+    
+    UART1_Init(); // Inizializza UART1
+    //UART1_WriteChar((char)chip_id);
+    
+    tmr_setup_period(TIMER2, 100);
+    
+    while(1){
+        spi_write_2_reg(read_addr, &value1, &value2);
+        value1 = value1 & 0x00F8;
+        value2 = value2 << 8;
+        value = value2 | value1;
+        value = value >> 3;
+
+       tmr_wait_period(TIMER2);
     }
     return 0;
 }
